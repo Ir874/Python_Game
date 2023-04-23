074831ada3dd63c3968a901d141e93e4c555559d
Ishaan Rao <ishaan_rao@icloud.com>
2023-04-22 21:38:47 -0400

    Added key input detection

--- main.py
+++ main.py
@@ -14,11 +14,28 @@ pygame.display.set_caption("Game")
  
 # Game loop
 while True:
-  screen.fill((0, 0, 0))
+  screen.fill((0, 0, 0)) # Setting screen color
   
+  # Event capture loop
   for event in pygame.event.get():
     if event.type == QUIT:
       pygame.quit()
+
+    if event.type == pygame.KEYDOWN: # Detecting Key input
+      
+      if event.key == pygame.K_d: # Detecting D key
+        print("D")
+      
+      if event.key == pygame.K_s: # Detecting S key
+        print("S")
+
+      if event.key == pygame.K_a: # Detecting A key
+        print("A")
+      
+      if event.key == pygame.K_j: # Detecting J key
+        print("J")
+
+    
 
 # Frame updater
 pygame.display.flip()
