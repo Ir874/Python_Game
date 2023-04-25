b5a2445a757db6c6ff4776fcd4bc33bcf332c11a
Ishaan Rao <ishaan_rao@icloud.com>
2023-04-24 21:07:23 -0400

    Added sprites to screen and made color changing keybinds

--- main.py
+++ main.py
@@ -8,15 +8,33 @@ import random
 # Pygame initialization
 pygame.init()
 
+# Initialize FPS clock
+clock = pygame.time.Clock()
+
 # Window startup code
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Game")
- 
+
+# Defining the FASD buttons 
+F = pygame.Surface((20, 20))
+A = pygame.Surface((20, 20))
+S = pygame.Surface((20, 20))
+D = pygame.Surface((20, 20)) 
+
+
+
+
 # Game loop
 while True:
-  screen.fill((0, 0, 0)) # Setting screen color
+  screen.fill((255, 255, 255)) # Setting screen color
   
+  # Put the FASD buttons onto the screen
+  screen.blit(D,(220,100))
+  screen.blit(A,(100,100))
+  screen.blit(S,(140,100))
+  screen.blit(F,(180,100))
+
   # Event capture loop
   for event in pygame.event.get():
     if event.type == QUIT:
@@ -26,19 +44,31 @@ while True:
     if event.type == pygame.KEYDOWN: # Detecting Key input
       
       if event.key == pygame.K_d: # Detecting D key
-        print("D")
-      
+        D.fill((255,255,0))
+        sleep(0.5)
+        D.fill((0,0,0))
+
       if event.key == pygame.K_s: # Detecting S key
-        print("S")
+        S.fill((0,0,255))
+        sleep(0.5)
+        S.fill((0,0,0))
 
       if event.key == pygame.K_a: # Detecting A key
-        print("A")
-      
-      if event.key == pygame.K_f: # Detecting J key
-        print("F")
+        A.fill((0,255,0))
+        sleep(0.5)
+        A.fill((0,0,0))
+
+      if event.key == pygame.K_f: # Detecting F key
+        F.fill((255,0,0))
+        sleep(0.5)
+        F.fill((0,0,0))
 
     
+    # Frame updater
+    pygame.display.update()
 
-# Frame updater
-pygame.display.flip()
+    # Fps setter
+    clock.tick(60)
+    
+
 
